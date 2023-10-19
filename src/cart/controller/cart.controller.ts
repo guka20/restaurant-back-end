02c64c7d530118c85fc04b6828d36c05421e44c0
guka20 <gukadavitadze23@gmail.com>
2023-10-20 02:18:29 +0400

    fix swagger

--- src/cart/controller/cart.controller.ts
+++ src/cart/controller/cart.controller.ts
@@ -1,10 +1,10 @@
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
-import { CartEntity } from 'src/cart/entity/cart.entity';
 import { CartService } from '../service/cart.service';
 
 @Controller('cart')
+@ApiTags('Cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
   @Get('all')
